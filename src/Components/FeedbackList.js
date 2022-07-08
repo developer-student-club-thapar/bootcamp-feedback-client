a24d3d14d94345b6aea4248f9d25c83901c3d035
Yashvardhan Arora <yash22arora@gmail.com>
2022-07-09 02:06:55 +0530

    Make sure that a row is always filled
    Signed-off-by: Yashvardhan Arora <yash22arora@gmail.com>

--- src/Components/FeedbackList.js
+++ src/Components/FeedbackList.js
@@ -53,16 +53,19 @@ const Feedbacks = [
 
 const FeedbackList = ({ feedbacks }) => {
   const colors = ["purple", "yellow", "green", "pink"];
-  const span = ["", "md:col-span-2"];
-  let color;
+  const spanList = ["", "md:col-span-2"];
+  let color,
+    span,
+    countPerRow = 0;
   return (
     <div className="p-12 grid md:grid-cols-4 grid-cols-1 gap-4">
       {Feedbacks.map((feedback, index) => {
-        color =
-          colors[Math.floor(Math.random() * colors.length)] +
-          " " +
-          span[Math.floor(Math.random() * span.length)];
+        countPerRow % 4 > 2
+          ? (span = "")
+          : (span = spanList[Math.floor(Math.random() * spanList.length)]);
+        color = colors[Math.floor(Math.random() * colors.length)] + " " + span;
 
+        countPerRow = span === "" ? countPerRow + 1 : countPerRow + 2;
         feedback.color = color;
         return <FeedbackItem feedback={feedback} />;
       })}
